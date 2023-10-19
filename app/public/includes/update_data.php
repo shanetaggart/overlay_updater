56fc30b5159fe17afef0a68922a99fd0800007d7
Shane Taggart <staggart@eastlink.ca>
2023-10-19 20:06:55 -0300

    Combined forms and form actions into one.

--- app/public/includes/update_data.php
+++ app/public/includes/update_data.php
@@ -2,36 +2,68 @@
 
 /**
  * Updates the match details by changing:
- *   -  left_score.txt
+ *   -  best_of.txt
  *   -  left_character.png
- *   -  right_score.txt
+ *   -  left_name.txt
+ *   -  left_score.txt
  *   -  right_character.png
+ *   -  right_name.txt
+ *   -  right_score.txt
+ *   -  set_name.txt
  */
 
 // Store all of the information from the form.
-$left_score = $_POST['left_score'];
+$best_of = $_POST['best_of'];
 $left_character = $_POST['left_character'];
-$right_score = $_POST['right_score'];
+$left_name = $_POST['left_name'];
+$left_score = $_POST['left_score'];
 $right_character = $_POST['right_character'];
+$right_name = $_POST['right_name'];
+$right_score = $_POST['right_score'];
+$set_name = $_POST['set_name'];
 
 // Store all of the file paths for the overlay files.
-$left_score_path = '../overlay_files/left_score.txt';
+$best_of_path = '../overlay_files/best_of.txt';
 $left_character_path = '../overlay_files/left_character.png';
-$right_score_path = '../overlay_files/right_score.txt';
+$left_name_path = '../overlay_files/left_name.txt';
+$left_score_path = '../overlay_files/left_score.txt';
 $right_character_path = '../overlay_files/right_character.png';
+$right_name_path = '../overlay_files/right_name.txt';
+$right_score_path = '../overlay_files/right_score.txt';
+$set_name_path = '../overlay_files/set_name.txt';
 
 // Store the file path for the character assets.
 $character_asset_path = '../assets/characters/';
 
 // Sanitize and convert text to uppercase.
+if ($best_of !== '') {
+    $best_of = trim($best_of);
+    $best_of = strtoupper($best_of);
+}
+
+if ($left_name !== '') {
+    $left_name = trim($left_name);
+    $left_name = strtoupper($left_name);
+}
+
 if ($left_score !== '') {
     $left_score = trim($left_score);
     $left_score = strtoupper($left_score);
 }
 
+if ($right_name !== '') {
+    $right_name = trim($right_name);
+    $right_name = strtoupper($right_name);
+}
+
 if ($right_score !== '') {
     $right_score = trim($right_score);
     $right_score = strtoupper($right_score);
+}
+
+if ($set_name !== '') {
+    $set_name = trim($set_name);
+    $set_name = strtoupper($set_name);
 }
 
 // Sanitize and convert character names to image file names.
@@ -48,14 +80,34 @@ if ($right_character !== '') {
 }
 
 // Open all of the text files that need updating.
+if ($best_of !== '' && file_exists($best_of_path)) {
+    $best_of_file = fopen($best_of_path, "w")
+        or die("Unable to open file best_of.txt!");
+}
+
+if ($left_name !== '' && file_exists($left_name_path)) {
+    $left_name_file = fopen($left_name_path, "w")
+        or die("Unable to open file left_name.txt!");
+}
+
 if ($left_score !== '' && file_exists($left_score_path)) {
     $left_score_file = fopen($left_score_path, "w")
         or die("Unable to open file left_score.txt!");
 }
 
+if ($right_name !== '' && file_exists($right_name_path)) {
+    $right_name_file = fopen($right_name_path, "w")
+        or die("Unable to open file right_name.txt!");
+}
+
 if ($right_score !== '' && file_exists($right_score_path)) {
     $right_score_file = fopen($right_score_path, "w")
         or die("Unable to open file right_score.txt!");
+}
+
+if ($set_name !== '' && file_exists($set_name_path)) {
+    $set_name_file = fopen($set_name_path, "w")
+        or die("Unable to open file set_name.txt!");
 }
 
 // Copy the image files and replace the placeholders.
@@ -76,21 +128,53 @@ if (
 }
 
 // Write the data to the appropriate text files.
+if (isset($best_of_file) && $best_of !== '') {
+    fwrite($best_of_file, $best_of);
+}
+
+if (isset($left_name_file) && $left_name !== '') {
+    fwrite($left_name_file, $left_name);
+}
+
 if (isset($left_score_file) && $left_score !== '') {
     fwrite($left_score_file, $left_score);
+}
+
+if (isset($right_name_file) && $right_name !== '') {
+    fwrite($right_name_file, $right_name);
 }
 
 if (isset($right_score_file) && $right_score !== '') {
     fwrite($right_score_file, $right_score);
 }
 
+if (isset($set_name_file) && $set_name !== '') {
+    fwrite($set_name_file, $set_name);
+}
+
 // Overwrite the text files and close them.
+if (isset($best_of_file)) {
+    fclose($best_of_file);
+}
+
+if (isset($left_name_file)) {
+    fclose($left_name_file);
+}
+
 if (isset($left_score_file)) {
     fclose($left_score_file);
 }
 
+if (isset($right_name_file)) {
+    fclose($right_name_file);
+}
+
 if (isset($right_score_file)) {
     fclose($right_score_file);
+}
+
+if (isset($set_name_file)) {
+    fclose($set_name_file);
 }
 
 // Redirect back to the main Overlay Updater page.
--- app/public/index.php
+++ app/public/index.php
@@ -2,19 +2,27 @@
 <html>
 
 <head>
+
 	<link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="./includes/main.css" />
 	<script type="text/javascript" src="./includes/characters.js"></script>
 	<script type="text/javascript" src="./includes/main.js"></script>
+
 </head>
 
 <body>
+
 	<section>
+
 		<h1>Overlay Updater</h1>
-		<form action="/includes/update_set.php" method="post">
+
+		<form action="/includes/update_data.php" method="post">
+
 			<h2>Set Details</h2>
+
 			<label for="set_name">Set Name</label>
 			<input type="text" name="set_name" id="set_name" maxlength="16" tabindex="1" />
+
 			<label for="best_of">Best Of</label>
 			<select name="best_of" id="best_of" tabindex="2">
 				<option value="" selected></option>
@@ -22,7 +30,9 @@
 				<option value="best of 3">Best of 3</option>
 				<option value="best of 5">Best of 5</option>
 			</select>
+
 			<section>
+
 				<aside>
 					<label for="left_name">Left Name</label>
 					<input type="text" name="left_name" id="left_name" tabindex="3" />
@@ -32,14 +42,13 @@
 					<label for="right_name">Right Name</label>
 					<input type="text" name="right_name" id="right_name" tabindex="4" />
 				</aside>
+
 			</section>
 
-			<input type="submit" value="Update" tabindex="5" />
-		</form>
-
-		<form action="/includes/update_match.php" method="post">
 			<h2>Match Details</h2>
+
 			<section>
+
 				<aside>
 					<label for="left_score">Left Score</label>
 					<input type="number" name="left_score" id="left_score" min="0" max="5" tabindex="6" />
@@ -53,10 +62,17 @@
 					<label for="right_character">Right Character</label>
 					<select class="characters" name="right_character" id="right_character" tabindex="9"></select>
 				</aside>
+
 			</section>
-			<input type="submit" value="Update" tabindex="10" />
+
+			<div class="sticky-submit-wrapper">
+				<input type="submit" value="Update" tabindex="10" />
+			</div>
+
 		</form>
+
 	</section>
+
 </body>
 
 </html>
