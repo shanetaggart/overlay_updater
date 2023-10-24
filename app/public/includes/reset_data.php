c646f7c7d816b0093d33b830259868b1ba209504
Shane Taggart <staggart@eastlink.ca>
2023-10-24 16:28:54 -0300

    Added placeholder functionality to form.

--- app/public/includes/reset_data.php
+++ app/public/includes/reset_data.php
@@ -3,9 +3,11 @@
 // Store all of the file paths for the overlay files.
 $best_of_path = '../overlay_files/best_of.txt';
 $left_character_path = '../overlay_files/left_character.png';
+$left_character_name_path = '../overlay_files/left_character_name.txt';
 $left_name_path = '../overlay_files/left_name.txt';
 $left_score_path = '../overlay_files/left_score.txt';
 $right_character_path = '../overlay_files/right_character.png';
+$right_character_name_path = '../overlay_files/right_character_name.txt';
 $right_name_path = '../overlay_files/right_name.txt';
 $right_score_path = '../overlay_files/right_score.txt';
 $set_name_path = '../overlay_files/set_name.txt';
@@ -13,9 +15,11 @@ $set_name_path = '../overlay_files/set_name.txt';
 // Create all necessary files in overlay_files.
 $best_of_file = fopen($best_of_path, 'w');
 $left_character_file = fopen($left_character_path, 'w');
+$left_character_name_file = fopen($left_character_name_path, 'w');
 $left_name_file = fopen($left_name_path, 'w');
 $left_score_file = fopen($left_score_path, 'w');
 $right_character_file = fopen($right_character_path, 'w');
+$right_character_name_file = fopen($right_character_name_path, 'w');
 $right_name_file = fopen($right_name_path, 'w');
 $right_score_file = fopen($right_score_path, 'w');
 $set_name_file = fopen($set_name_path, 'w');
@@ -30,27 +34,36 @@ fwrite($left_score_file, '0');
 
 // Create a 3D array of character duos/rivalries.
 $character_duos = [
-    ['mario.png', 'bowser.png'],
-    ['cloud.png', 'sephiroth.png'],
-    ['pit.png', 'darkpit.png'],
-    ['samus.png', 'darksamus.png'],
-    ['donkeykong.png', 'kingkrool.png'],
-    ['fox.png', 'wolf.png'],
-    ['kirby.png', 'kingdedede.png'],
-    ['link.png', 'ganondorf.png'],
-    ['kirby.png', 'metaknight.png'],
-    ['luigi.png', 'piranhaplant.png'],
-    ['pokemontrainer.png', 'mewtwo.png'],
-    ['samus.png', 'ridley.png'],
-    ['yoshi.png', 'bowserjunior.png']
+    ['chrom', 'robin'],
+    ['cloud', 'sephiroth'],
+    ['diddykong', 'kingkrool'],
+    ['donkeykong', 'kingkrool'],
+    ['fox', 'wolf'],
+    ['kirby', 'kingdedede'],
+    ['kirby', 'metaknight'],
+    ['link', 'ganondorf'],
+    ['luigi', 'piranhaplant'],
+    ['mario', 'bowser'],
+    ['mario', 'wario'],
+    ['pit', 'darkpit'],
+    ['pokemontrainer', 'mewtwo'],
+    ['ryu', 'ken'],
+    ['samus', 'darksamus'],
+    ['samus', 'ridley'],
+    ['toonlink', 'ganondorf'],
+    ['yoshi', 'bowserjunior'],
+    ['younglink', 'ganondorf']
 ];
 
 // Choose a random duo/rivalry.
 $duo = rand(0, count($character_duos));
 
 // Set the default images for the left and right characters to the randomly chosen duo/rivalry.
-copy('../assets/characters/left/' . $character_duos[$duo][0], '../overlay_files/left_character.png');
-copy('../assets/characters/right/' . $character_duos[$duo][1], '../overlay_files/right_character.png');
+copy('../assets/characters/left/' . $character_duos[$duo][0] . '.png', '../overlay_files/left_character.png');
+copy('../assets/characters/right/' . $character_duos[$duo][1] . '.png', '../overlay_files/right_character.png');
+
+fwrite($left_character_name_file, $character_duos[$duo][0]);
+fwrite($right_character_name_file, $character_duos[$duo][1]);
 
 // Close all open streams.
 array_map('fclose', get_resources('stream'));
