678a27a6a3c1e3e506d6851d96458b986e063438
Shane Taggart <staggart@eastlink.ca>
2023-12-01 19:13:14 -0400

    Added commentary names and various bug fixes.

---
+++ app/public/includes/blocks/nav.php
@@ -0,0 +1,5 @@
+<nav class="breadcrumb">
+    <a href="index.php">Overlay Updater</a>
+    <a href="player_names.php">Set Player Names</a>
+    <a href="commentator_names.php">Set Commentator Names</a>
+</nav>
